7185d04747a410729d079d2014fa7089119ee7e1
Lord USBQ <buendiau@gmail.com>
2025-06-30 08:35:49 -0600

    Pushing last version June 30

--- Tests/Test_home_page.py
+++ Tests/Test_home_page.py
@@ -10,9 +10,8 @@ class Test_Home_Page:
         """Test the list of available articles"""
         article_list = ["Sauce Labs Backpack","Sauce Labs Bike Light","Sauce Labs Bolt T-Shirt","Sauce Labs Fleece Jacket","Sauce Labs Onesie","Test.allTheThings() T-Shirt (Red)"]
         driver = driver_setup
-        l_page = LoginPage(driver)
-        l_page.login()
         h_page = HomePage(driver)
+        h_page.login()
         articles = h_page.get_article_list()
         for i,article in enumerate(articles):
             assert article.text == article_list[i], f"Article {i+1} does not match expected value. Expected: {article_list[i]}, Found: {article}"
